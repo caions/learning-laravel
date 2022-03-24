6d5ab20d7a711a45a7638a483814110e0c64bd9a
Caio Nascimento <caionsantos2000@gmail.com>
2022-03-23 22:24:03 -0300

    feat: logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         <a href="" class="p-3">Home</a>
       </li>
       <li>
-        <a href="" class="p-3">Dashboard</a>
+        <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
       </li>
       <li>
         <a href="" class="p-3">Post</a>
@@ -31,7 +31,10 @@
         </a>
       </li>
       <li>
-        <a href="" class="p-3">Logout</a>
+        <form action="{{route('logout')}}" method="post" class="p-3 inline">
+          @csrf
+          <button type="submit">Logout</button>
+        </form>
       </li>
       @endauth
 
