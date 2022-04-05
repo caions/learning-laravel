5a27193e3f763c81627bacc81fc1b52b949edde7
Caio Nascimento <caionsantos2000@gmail.com>
2022-04-05 17:28:25 -0300

    feat: post form

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,6 +34,5 @@ Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/posts', function () {
-  return view('posts.index');
-});
+Route::get('/post', [PostController::class, 'index'])->name('posts');
+Route::post('/post', [PostController::class, 'store']);
