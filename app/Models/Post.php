bf416fb05afec7f78f0e665fe8e0ee6dfe603924
Caio Nascimento <caionsantos2000@gmail.com>
2022-04-06 15:07:15 -0300

    feat: get posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-        'body'
-    ];
+  protected $fillable = ['body'];
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
